b2dec0c9bce6ef8936b7d7e9ca1da814b13ef439
YasinSeyhun <seyhunyasin@gmail.com>
2024-01-07 23:53:40 +0300

    y

--- JavaScript/13_donguler.js
+++ JavaScript/13_donguler.js
@@ -39,4 +39,40 @@ while (i < 10) {
 
 // do while loop
 
+i = 0 ;
+
+do {
+  console.log(i);
+  i++;
+} while (i<10);
+
 // for loop
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+  
+  
+}
+
+for (let i = 0; i < 10; i++) {
+  if (i == 3) {
+    console.log("Seçilen rakam : " + i);
+    break; // seçilen rakamdan sonra döngü bitiyor.
+  }
+  console.log(i);
+  
+}
+
+let toplam = 0;
+
+for (let i = 1; i < 10; i++) {
+  toplam += i;
+  console.log(toplam);
+  
+}
+
+let sonuc = 1;
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
