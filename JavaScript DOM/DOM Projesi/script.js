a7102f853de61bcfad6500dc8f27034a9dfc8727
YasinSeyhun <seyhunyasin@gmail.com>
2024-07-01 19:01:12 +0300

    u

--- JavaScript DOM/DOM Projesi/script.js
+++ JavaScript DOM/DOM Projesi/script.js
@@ -8,26 +8,34 @@ const input = document.querySelector("#txtTaskName");
 const btnAddNewTask = document.querySelector("#btnAddNewTask");
 const btnDeleteAll = document.querySelector("#btnDeleteAll");
 const taskList = document.querySelector("#task-list");
+const items = ["Todo1","Todo2","Todo3","Todo4","Todo5",];
 
+// load items
+loadItems();
 
 eventListeners();
 
 function eventListeners(){
+    //submit event
     form.addEventListener("submit",addNewItem);
-
+    //delete event
+    taskList.addEventListener("click",deleteItem);
+    //delete all event
+    btnDeleteAll.addEventListener("click",deleteAllItem)
 }
 
-function addNewItem(e){
-    if(input.value ===''){
-        alert("Deger Giriniz");
-        //console.log("submit");
-    }
+function loadItems() {
+    items.forEach(function(item){
+        createItems(item);
+    })
+}
 
-    // li oluşturma
+function createItems(text) {
+     // li oluşturma
 
     const li = document.createElement("li");
     li.className = "list-group-item list-group-item-secondary";
-    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(document.createTextNode(text));
 
     // a oluşturma
 
@@ -39,12 +47,48 @@ function addNewItem(e){
     li.appendChild(a);
 
     taskList.appendChild(li);
-    
+}
+
+function addNewItem(e){
+    if(input.value ===''){
+        alert("Deger Giriniz");
+        //console.log("submit");
+    }
+
     input.value = "";
 
     e.preventDefault();
-
 }
+
+// Eleman Silme
+function deleteItem(e) {
+
+    if (confirm("Silmek istediğinize emin misiniz?")) {
+        if (e.target.className === "fas fa-times"){
+           // console.log(e.target);
+           e.target.parentElement.parentElement.remove();
+        }
+    }
+        
+    e.preventDefault();    
+    }
+
+// Tüm elemanları silme
+function deleteAllItem(e) {
+    if (confirm("Silmek istediğinize emin misiniz?")) {
+        taskList.childNodes.forEach(function(item){
+            //console.log(item);
+            if(item.nodeType === 1){
+                item.remove();
+            }
+        })
+    }
+    // taskList.innerHTML=""; (baska bir yontem)
+}
+
+
+
+
 
 
 
